72ac12ad6758bd59f1e346bebce7490807589c36
Markus <markushh@uio.no>
2025-04-04 15:37:49 +0200

    WIP

--- benches/diffsol.rs
+++ benches/diffsol.rs
@@ -1,34 +1,58 @@
-use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
-type M = nalgebra::DMatrix<f64>;
-type V = nalgebra::DVector<f64>;
-type S = f64;
+use pmcore::prelude::*;
+fn ode() -> ODE {
+    equation::ODE::new(
+        |x, p, t, dx, rateiv, cov| {
+            fetch_params!(p, cls, k30, k40, qs, vps, vs, fm1, fm2, theta1, theta2);
+            fetch_cov!(cov, t, wt, pkvisit);
 
-// lotka_volterra
-//  F: Fn(&M::V, &M::V, M::T, &mut M::V),
-fn rhs() -> impl Fn(&V, &V, S, &mut V) {
-    |x: &V, p: &V, _t: S, y: &mut V| {
-        if p.len() != 4 {
-            dbg!(&p);
-        }
-        let alpha: f64 = p[0];
-        let beta = p[1];
-        let delta = p[2];
-        let gamma = p[3];
+            let vfrac1 = 0.068202;
+            let vfrac2 = 0.022569;
+            let cl = cls * ((pkvisit - 1.0) * theta1).exp() * (wt / 70.0).powf(0.75);
+            let q = qs * (wt / 70.0).powf(0.75);
+            let v = vs * ((pkvisit - 1.0) * theta2).exp() * (wt / 70.0);
+            let vp = vps * (wt / 70.0);
+            let ke = cl / v;
+            let _vm1 = vfrac1 * v;
+            let _vm2 = vfrac2 * v;
+            let k12 = q / v;
+            let k21 = q / vp;
 
-        y[0] = alpha * x[0] - beta * x[0] * x[1];
-        y[1] = delta * x[0] * x[1] - gamma * x[1];
-    }
-}
-fn jac() -> impl Fn(&V, &V, S, &V, &mut V) {
-    |x: &V, p: &V, _t: S, v: &V, y: &mut V| {
-        let alpha = p[0];
-        let beta = p[1];
-        let delta = p[2];
-        let gamma = p[3];
+            //</tem>
+            dx[0] = rateiv[0] - ke * x[0] * (1.0 - fm1 - fm2) - (fm1 + fm2) * x[0] - k12 * x[0]
+                + k21 * x[1];
+            dx[1] = k12 * x[0] - k21 * x[1];
+            dx[2] = fm1 * x[0] - k30 * x[2];
+            dx[3] = fm2 * x[0] - k40 * x[3];
+        },
+        |_p| {
+            lag! {}
+        },
+        |_p| {
+            fa! {}
+        },
+        |_p, _t, _cov, _x| {},
+        |x, p, t, cov, y| {
+            fetch_params!(p, cls, _k30, _k40, qs, vps, vs, _fm1, _fm2, theta1, theta2);
+            fetch_cov!(cov, t, wt, pkvisit);
 
-        y[0] = (alpha - beta * x[1]) * v[0] - beta * x[0] * v[1];
-        y[1] = delta * x[1] * v[0] + (delta * x[0] - gamma) * v[1];
-    }
+            let vfrac1 = 0.068202;
+            let vfrac2 = 0.022569;
+            let cl = cls * ((pkvisit - 1.0) * theta1).exp() * (wt / 70.0).powf(0.75);
+            let q = qs * (wt / 70.0).powf(0.75);
+            let v = vs * ((pkvisit - 1.0) * theta2).exp() * (wt / 70.0);
+            let vp = vps * (wt / 70.0);
+            let _ke = cl / v;
+            let vm1 = vfrac1 * v;
+            let vm2 = vfrac2 * v;
+            let _k12 = q / v;
+            let _k21 = q / vp;
+
+            y[0] = x[0] / v;
+            y[1] = x[2] / vm1;
+            y[2] = x[3] / vm2;
+        },
+        (4, 3),
+    )
 }
 
 fn run_diffsol_053(n: usize) {
@@ -65,7 +89,7 @@ fn run_diffsol_030(n: usize) {
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Comparison");
-    for &n in &[1, 2, 4, 8, 16, 32, 64] {
+    for &n in &[2048, 4096] {
         group.bench_with_input(BenchmarkId::new("Diffsol 0.5.3", n), &n, |b, &n| {
             b.iter(|| run_diffsol_053(black_box(n)))
         });
